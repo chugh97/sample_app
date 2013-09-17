27ae774ecd9823b69eadf7eef407c8898a7a9dfb
Shaleen Chugh <shaleen.chugh@sage.com>
2013-09-17 14:05:42 +0100

    Finished signin

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,7 @@ describe User do
     it { should respond_to(:password) }
     it { should respond_to(:password_confirmation) }
     it {should respond_to(:authenticate)}
+    it { should respond_to(:remember_token) }
     
     it { should be_valid }
     
@@ -77,14 +78,16 @@ describe User do
         end
         
         describe "with invalid password" do
-            let(:user_for_invalid_password) {found_user.authenticate("inalid")}
+            let(:user_for_invalid_password) {found_user.authenticate("invalid")}
             
             it { should_not eq user_for_invalid_password}
             specify { expect(user_for_invalid_password).to be_false}
         end
-        
-        
-         
+    end
+    
+    describe "remember token" do
+        before { @user.save }
+        its(:remember_token) { should_not be_blank }
     end
 
 end
